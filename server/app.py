bf0026d21a08cd27ee026f13bf341516278c2fa2
Mercy <mercymumbe17@gmail.com>
2025-06-13 12:33:18 +0300

    all tests passing

--- server/app.py
+++ server/app.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 from models import db, Earthquake
 
+# Initialize the app
 app = Flask(__name__)
+
+# Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# Initialize extensions
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# Index route
 @app.route('/')
 def index():
     return make_response({'message': 'Flask SQLAlchemy Lab 1'}, 200)
 
+# Task #3: Get earthquake by ID
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    if quake:
+        return jsonify(quake.to_dict()), 200
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
+
+# Task #4: Get earthquakes matching magnitude
 @app.route('/earthquakes/magnitude/<float:magnitude>')
 def earthquakes_by_magnitude(magnitude):
     quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
@@ -24,5 +39,6 @@ def earthquakes_by_magnitude(magnitude):
         "quakes": [quake.to_dict() for quake in quakes]
     }), 200
 
+# Run the server
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
